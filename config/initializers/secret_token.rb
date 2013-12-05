0a5190b2fa8eff56f80214a262cc2a81c9c426f4
Christoph Edthofer <wukerplank@edthofer.at>
2013-12-05 10:08:02 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skelight::Application.config.secret_key_base = '9a7d2981f7ce7349a03893ab827181ce90e67a3d970101b47d47152d3b55946d127ce777a173904510277b85bfa83f2edf706593328bd220e3be87fa10b6a9dd'
